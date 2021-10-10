ff6b0185a78be76a28b113c0a52175a51bac0da1
unknown <youssef.j.houssaf@gmail.com>
2021-10-10 17:07:32 +0100

    hgh

--- Testapp/Form1.Designer.cs
+++ Testapp/Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace Testapp
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -104,6 +105,7 @@ namespace Testapp
             // 
             // panel5
             // 
+            this.panel5.Controls.Add(this.button1);
             this.panel5.Controls.Add(this.btn_delete_article);
             this.panel5.Controls.Add(this.btn_add_article);
             this.panel5.Controls.Add(this.btn_modifier_article);
@@ -214,6 +216,15 @@ namespace Testapp
             this.dateTimePicker1.Size = new System.Drawing.Size(273, 24);
             this.dateTimePicker1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -252,6 +263,7 @@ namespace Testapp
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
