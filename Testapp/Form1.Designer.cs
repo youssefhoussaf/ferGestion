081e708b6761836b5b0748f64214057891d97818
houssaf <youssef.j.houssaf@gmail.com>
2021-10-22 22:17:49 +0100

    gg

--- Testapp/Form1.Designer.cs
+++ Testapp/Form1.Designer.cs
@@ -38,13 +38,23 @@ namespace Testapp
             this.btn_ventes = new System.Windows.Forms.Button();
             this.btn_articles = new System.Windows.Forms.Button();
             this.container = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.headerBar = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_maxi = new System.Windows.Forms.Button();
+            this.btn_mini = new System.Windows.Forms.Button();
+            this.lbl_title = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.headerBar.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btn_types);
@@ -53,18 +63,16 @@ namespace Testapp
             this.panel1.Controls.Add(this.btn_articles);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(379, 949);
+            this.panel1.Size = new System.Drawing.Size(284, 771);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(9, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 80);
+            this.pictureBox1.Size = new System.Drawing.Size(94, 65);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -74,24 +82,23 @@ namespace Testapp
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Gray;
-            this.label1.Location = new System.Drawing.Point(143, 39);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(107, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 31);
+            this.label1.Size = new System.Drawing.Size(156, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "PF Aluminium";
             // 
             // btn_types
             // 
-            this.btn_types.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_types.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.btn_types.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_types.FlatAppearance.BorderSize = 0;
             this.btn_types.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_types.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_types.ForeColor = System.Drawing.Color.Black;
-            this.btn_types.Location = new System.Drawing.Point(0, 378);
-            this.btn_types.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_types.ForeColor = System.Drawing.Color.White;
+            this.btn_types.Location = new System.Drawing.Point(0, 307);
             this.btn_types.Name = "btn_types";
-            this.btn_types.Size = new System.Drawing.Size(379, 79);
+            this.btn_types.Size = new System.Drawing.Size(284, 64);
             this.btn_types.TabIndex = 3;
             this.btn_types.Text = "Types d\'article";
             this.btn_types.UseVisualStyleBackColor = false;
@@ -99,75 +106,167 @@ namespace Testapp
             // 
             // btn_achats
             // 
-            this.btn_achats.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_achats.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.btn_achats.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_achats.FlatAppearance.BorderSize = 0;
             this.btn_achats.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_achats.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_achats.ForeColor = System.Drawing.Color.Black;
-            this.btn_achats.Location = new System.Drawing.Point(0, 302);
-            this.btn_achats.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_achats.ForeColor = System.Drawing.Color.White;
+            this.btn_achats.Location = new System.Drawing.Point(0, 245);
             this.btn_achats.Name = "btn_achats";
-            this.btn_achats.Size = new System.Drawing.Size(379, 79);
+            this.btn_achats.Size = new System.Drawing.Size(284, 64);
             this.btn_achats.TabIndex = 2;
             this.btn_achats.Text = "Achats";
             this.btn_achats.UseVisualStyleBackColor = false;
             // 
             // btn_ventes
             // 
-            this.btn_ventes.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_ventes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.btn_ventes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ventes.FlatAppearance.BorderSize = 0;
             this.btn_ventes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_ventes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_ventes.ForeColor = System.Drawing.Color.Black;
-            this.btn_ventes.Location = new System.Drawing.Point(0, 224);
-            this.btn_ventes.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ventes.ForeColor = System.Drawing.Color.White;
+            this.btn_ventes.Location = new System.Drawing.Point(0, 182);
             this.btn_ventes.Name = "btn_ventes";
-            this.btn_ventes.Size = new System.Drawing.Size(379, 79);
+            this.btn_ventes.Size = new System.Drawing.Size(284, 64);
             this.btn_ventes.TabIndex = 1;
             this.btn_ventes.Text = "Ventes";
             this.btn_ventes.UseVisualStyleBackColor = false;
             this.btn_ventes.Click += new System.EventHandler(this.btn_ventes_Click);
+            this.btn_ventes.MouseEnter += new System.EventHandler(this.btn_ventes_MouseEnter);
+            this.btn_ventes.MouseLeave += new System.EventHandler(this.btn_ventes_MouseLeave);
             // 
             // btn_articles
             // 
-            this.btn_articles.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_articles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.btn_articles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_articles.FlatAppearance.BorderSize = 0;
             this.btn_articles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_articles.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_articles.ForeColor = System.Drawing.Color.Black;
-            this.btn_articles.Location = new System.Drawing.Point(0, 146);
-            this.btn_articles.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_articles.ForeColor = System.Drawing.Color.White;
+            this.btn_articles.Location = new System.Drawing.Point(0, 119);
             this.btn_articles.Name = "btn_articles";
-            this.btn_articles.Size = new System.Drawing.Size(379, 79);
+            this.btn_articles.Size = new System.Drawing.Size(284, 64);
             this.btn_articles.TabIndex = 0;
             this.btn_articles.Text = "Articles";
             this.btn_articles.UseVisualStyleBackColor = false;
             this.btn_articles.Click += new System.EventHandler(this.btn_articles_Click);
+            this.btn_articles.MouseEnter += new System.EventHandler(this.btn_articles_MouseEnter);
+            this.btn_articles.MouseLeave += new System.EventHandler(this.btn_articles_MouseLeave);
             // 
             // container
             // 
-            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.container.Location = new System.Drawing.Point(379, 0);
-            this.container.Margin = new System.Windows.Forms.Padding(4);
+            this.container.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.container.Location = new System.Drawing.Point(0, 73);
             this.container.Name = "container";
-            this.container.Size = new System.Drawing.Size(1369, 949);
+            this.container.Size = new System.Drawing.Size(1027, 698);
             this.container.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.headerBar);
+            this.panel2.Controls.Add(this.container);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(284, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1027, 771);
+            this.panel2.TabIndex = 2;
+            // 
+            // headerBar
+            // 
+            this.headerBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
+            this.headerBar.Controls.Add(this.panel3);
+            this.headerBar.Controls.Add(this.lbl_title);
+            this.headerBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerBar.Location = new System.Drawing.Point(0, 0);
+            this.headerBar.Name = "headerBar";
+            this.headerBar.Size = new System.Drawing.Size(1027, 77);
+            this.headerBar.TabIndex = 2;
+            this.headerBar.Paint += new System.Windows.Forms.PaintEventHandler(this.headerBar_Paint);
+            this.headerBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.headerBar_MouseDown);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btn_close);
+            this.panel3.Controls.Add(this.btn_maxi);
+            this.panel3.Controls.Add(this.btn_mini);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(924, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(103, 77);
+            this.panel3.TabIndex = 1;
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_close.BackgroundImage")));
+            this.btn_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatAppearance.BorderSize = 0;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Location = new System.Drawing.Point(71, 6);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(20, 20);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_maxi
+            // 
+            this.btn_maxi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_maxi.BackgroundImage")));
+            this.btn_maxi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_maxi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_maxi.FlatAppearance.BorderSize = 0;
+            this.btn_maxi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_maxi.Location = new System.Drawing.Point(38, 6);
+            this.btn_maxi.Name = "btn_maxi";
+            this.btn_maxi.Size = new System.Drawing.Size(20, 20);
+            this.btn_maxi.TabIndex = 1;
+            this.btn_maxi.UseVisualStyleBackColor = true;
+            this.btn_maxi.Click += new System.EventHandler(this.btn_maxi_Click);
+            // 
+            // btn_mini
+            // 
+            this.btn_mini.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_mini.BackgroundImage")));
+            this.btn_mini.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_mini.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_mini.FlatAppearance.BorderSize = 0;
+            this.btn_mini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_mini.Location = new System.Drawing.Point(4, 6);
+            this.btn_mini.Name = "btn_mini";
+            this.btn_mini.Size = new System.Drawing.Size(20, 20);
+            this.btn_mini.TabIndex = 0;
+            this.btn_mini.UseVisualStyleBackColor = true;
+            this.btn_mini.Click += new System.EventHandler(this.btn_mini_Click);
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.White;
+            this.lbl_title.Location = new System.Drawing.Point(28, 21);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(0, 24);
+            this.lbl_title.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1748, 949);
-            this.Controls.Add(this.container);
+            this.ClientSize = new System.Drawing.Size(1311, 771);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.headerBar.ResumeLayout(false);
+            this.headerBar.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -182,6 +281,13 @@ namespace Testapp
         private System.Windows.Forms.Button btn_achats;
         private System.Windows.Forms.Button btn_ventes;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel headerBar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_maxi;
+        private System.Windows.Forms.Button btn_mini;
+        private System.Windows.Forms.Label lbl_title;
     }
 }
 
--- Testapp/Form1.cs
+++ Testapp/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace Testapp
 {
@@ -16,7 +17,17 @@ namespace Testapp
         public Form1()
         {
             InitializeComponent();
+            this.ControlBox = false;
+            this.Text = string.Empty;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
         }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+
 
         private SqlConnection cnx = new Connexion().cnx;
 
@@ -53,6 +64,55 @@ namespace Testapp
             type.Dock = DockStyle.Fill;
             type.frm = this;
             container.Controls.Add(type);
+        }
+
+        private void btn_articles_MouseEnter(object sender, EventArgs e)
+        {
+            btn_articles.BackColor = Color.FromArgb(105, 105, 155);
+        }
+
+        private void btn_articles_MouseLeave(object sender, EventArgs e)
+        {
+            btn_articles.BackColor = Color.FromArgb(52,52,76);
+        }
+
+        private void headerBar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void headerBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle,0x112,0xf012,0);
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_maxi_Click(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Normal;
+            else
+                this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btn_mini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_ventes_MouseEnter(object sender, EventArgs e)
+        {
+            btn_ventes.BackColor = Color.FromArgb(105, 105, 155);
+        }
+
+        private void btn_ventes_MouseLeave(object sender, EventArgs e)
+        {
+            btn_ventes.BackColor = Color.FromArgb(52, 52, 76);
         }
     }
 }
